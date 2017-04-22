0516911b75ca9b15c56a7240a44d3e853f61d5b2
Ewerton Costa <ewertonsjp@gmail.com>
2017-04-21 23:03:10 -0300

    Create schedule component

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { SchedulePage } from '../schedule/schedule';
 
 @Component({
   templateUrl: 'tabs.html'
 })
+
 export class TabsPage {
 
-  tab1Root = HomePage;
+  tab1Root = SchedulePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
